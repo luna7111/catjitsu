a5e49ef40d94e828324f5f61b857a57a29338e78
Luna <luna7111@tuta.io>
2026-05-17 11:28:11 +0200

    feat: added scene manager

---
+++ catjitsu/scene_manager.gd
@@ -0,0 +1,42 @@
+class_name SceneManager
+extends Node
+## Manager for the game scenes
+##
+## The SceneManager node is pointed to by the scene_manager variable in the
+## Global node.
+## Use Global.scene_manager.switch_scenes() to switch scenes
+
+@export var startup_scene: PackedScene
+var _current_scene: Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Global.scene_manager = self
+	_set_startup_scene(startup_scene)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _set_startup_scene(scene):
+	var new_scene = scene.instantiate()
+	add_child(new_scene)
+	_current_scene = new_scene
+
+
+## Switches the current scene to the one in the given path.
+## 
+## The persistence argument defines whether the scene will be kept in memory or
+## not, recurrent scenes like main menu should be kept in memory, one time
+## scenes like login/register scenes shouldn't.
+func switch_scene(path: String, persistence: bool):
+	if not persistence:
+		_current_scene.queue_free()
+	else:
+		remove_child(_current_scene)
+	var new_scene = load(path).instantiate()
+	add_child(new_scene)
+	_current_scene = new_scene
