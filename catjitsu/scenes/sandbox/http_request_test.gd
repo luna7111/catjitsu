1dceafe5a12a3c09622b78ae7165e5a266458c7b
Luna <luna7111@tuta.io>
2026-05-18 17:05:52 +0200

    lab: added test request to pokeapi

---
+++ catjitsu/scenes/sandbox/http_request_test.gd
@@ -0,0 +1,22 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_pressed() -> void:
+	$HTTPRequest.request_completed.connect(_on_request_completed)
+	$HTTPRequest.request("https://pokeapi.co/api/v2/pokemon/ditto")
+
+func _on_request_completed(result, response_code, headers, body):
+	var json = JSON.parse_string(body.get_string_from_utf8())
+	print(json["name"])
+	print(json["abilities"].size())
+	print(json["abilities"][0]["ability"]["name"])
