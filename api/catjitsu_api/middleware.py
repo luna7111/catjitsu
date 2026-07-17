b77e0305d4b73d26010f9a659bcfc6264b09d303
kde-la-c <kdelacolinaplant@gmail.com>
2026-07-17 16:16:54 +0200

    fixed some errors and updated settings to serve game, doesn't work yet

---
+++ api/catjitsu_api/middleware.py
@@ -0,0 +1,11 @@
+# catjitsu_api/middleware.py
+class GodotWasmHeaderMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        # These two headers allow Godot 4's Wasm threads to initialize
+        response["Cross-Origin-Opener-Policy"] = "same-origin"
+        response["Cross-Origin-Embedder-Policy"] = "require-corp"
+        return response
