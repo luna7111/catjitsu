e94f1e0707be3bab9e8ab0d8edf16aa6575a3b7c
kde-la-c <kdelacolinaplant@gmail.com>
2026-08-05 15:53:41 +0000

    prepared project for virtualization and added Dockerfile

--- api/catjitsu_api/settings.py
+++ api/catjitsu_api/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
-from django.conf.global_settings import CSRF_TRUSTED_ORIGINS
+from django.conf.global_settings import CSRF_TRUSTED_ORIGINS, STATIC_ROOT
 from dotenv import load_dotenv
 
 load_dotenv('.env.local')
@@ -161,6 +161,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
